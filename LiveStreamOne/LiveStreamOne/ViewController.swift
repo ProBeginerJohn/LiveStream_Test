0f4a2c045012c5a65025551542d866a9ac4c2b30
라경호 <gyeongho@lagyeongho-macbook-pro.local>
2019-05-17 15:06:51 +0800

    first

---
+++ LiveStreamOne/LiveStreamOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiveStreamOne
+//
+//  Created by 라경호 on 2019/5/17.
+//  Copyright © 2019 Johns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
